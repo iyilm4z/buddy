240e500076d6b044f65fec1adfe43dcd13425090
ismail.yilmaz <ismail.yilmaz@invicti.com>
2022-06-24 18:39:04 +0300

    Add generic host to wpf and setup Buddy

--- src/ui/wpf/Buddy.Desktop.Wpf/App.xaml.cs
+++ src/ui/wpf/Buddy.Desktop.Wpf/App.xaml.cs
@@ -1,6 +1,61 @@
-﻿namespace Buddy.Desktop
+﻿using System;
+using System.Windows;
+using Autofac.Extensions.DependencyInjection;
+using Buddy.Desktop.Views;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Buddy.Desktop
 {
     public partial class App
     {
+        private IHost _host = null!;
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddSingleton<MainWindow>();
+
+                    services.AddBuddy<BuddyDesktopWpfModule>();
+                })
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    var env = hostContext.HostingEnvironment;
+
+                    config.AddEnvironmentVariables();
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true,
+                        reloadOnChange: true);
+                });
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            _host = CreateHostBuilder(e.Args).Build();
+
+            _host.Services.UseBuddy();
+
+            await _host.StartAsync();
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+
+            base.OnExit(e);
+        }
     }
 }
